78e73788e41008f0591801c36608aa2a41d3ab19
Shreyasreddy2006 <shreyas.24bcs10152@sst.scaler.com>
2025-06-04 14:49:34 +0530

    url updated

--- server/ws-server.js
+++ server/ws-server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 1021;
 const wss = new WebSocket.Server({port : port});
 
 wss.on('connection' , (ws) => {
-
+   console.log(`${ws}`);
    ws.on('message' , (msg) => {
       wss.clients.forEach((client) => {
          if(client !== ws && client.readyState === WebSocket.OPEN){
